2e70028fc1ef950fdfa367331b872c547b82740b
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-03-21 21:54:26 -0300

    Initial commit rick and morty

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export function App() {
+
+  return (
+    <>
+      <div className="container">
+        <nav className='p-4 bg-slate-600'>
+          <p className='text-xl font-bold text-white'>Rick and Morty</p>
+        </nav>
+      </div>
+      <h1>Teste</h1>
+    </>
+  )
+}
+
